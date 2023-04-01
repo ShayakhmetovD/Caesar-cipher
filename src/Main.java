255d0a243a6c2a88188ecce06526d81da686581c
ShayakhmetovD <darkus747474@gmail.com>
2023-04-01 21:39:20 +0300

    1 этап

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,14 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.Scanner;
 
+/* Данная программа реализует шифр Цезаря на основе нескольких способов, а именно:
+1. Шифрование/расшифрование файла с текстом;
+2. Расшифрование файла способом "грубой силы" brute force;
+3. Расшифрование файла способом, на основе сбора статистических данных;
+ */
+
 public class Main {
+    char[] sym = {'.',',','"',':','-','!','?',' '};
     public static void main(String[] args) {
         try(Scanner scanner = new Scanner(System.in)){
             System.out.print("Введите путь к файлу: ");
@@ -16,7 +23,7 @@ public class Main {
 
     }
 
-
+    //Данный метод производит чтение из файла и выдает исходный текст
     public static String readingFromAFile(String path) {
         String sourceText = null;
         try (FileReader fileReader = new FileReader(path);
@@ -32,17 +39,68 @@ public class Main {
         return sourceText;
     }
 
-    public  static void encryption(String message, int key){
+    //Данный метод производит шифрование исходного текста
+    public  static StringBuilder encryption(String message, int key){
 
         char[] array = message.toCharArray();
         StringBuilder result = new StringBuilder();
         for (int i = 0; i < array.length; i++) {
-            char c = (char)(message.charAt(i) + key);
-            if (c > 'я')
-                result.append((char) (message.charAt(i) - (32 - key)));
-            else
-                result.append((char) (message.charAt(i) + key));
+            char symbol = message.charAt(i);
+            if((symbol >= 'а') && (symbol <= 'я')) // если символ в нижнем регистре
+            {
+                symbol = (char)(symbol + (key % 32)); // ключ % 32 бит
+                if(symbol < 'а') {
+                    symbol = (char) (symbol + 32); // если символ стоит левее, чем начало алфавита
+                }
+                if(symbol > 'я') {
+                    symbol = (char) (symbol - (key % 32));// если символ стоит правее, чем конец алфавита
+                }
+            }else if((symbol >= 'А') && (symbol <= 'Я'))// если символ в верхнем регистре (далее аналогично)
+            {
+                symbol = (char)(symbol + (key % 32));
+                if(symbol < 'А') {
+                    symbol = (char) (symbol + (key % 32));
+                }
+                if(symbol > 'Я') {
+                    symbol = (char) (symbol - (key % 32));
+                }
+            }
+            result.append(symbol);// Добавляем зашифрованные символы
         }
-        System.out.println(result);
+        return result;
         }
+
+    //Данный метод производит расшифрование зашифрованного текста
+    public  static StringBuilder decryption(String message, int key2){
+        int key = -key2;
+        char[] array = message.toCharArray();
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < array.length; i++) {
+            char symbol = message.charAt(i);
+            if((symbol >= 'а') && (symbol <= 'я')) // если символ в нижнем регистре
+            {
+                symbol = (char)(symbol + (key % 32)); // ключ % 32 бит
+                if(symbol < 'а') {
+                    symbol = (char) (symbol + 32); // если символ стоит левее, чем начало алфавита
+                }
+                if(symbol > 'я') {
+                    symbol = (char) (symbol - (key % 32));// если символ стоит правее, чем конец алфавита
+                }
+            }else if((symbol >= 'А') && (symbol <= 'Я'))// если символ в верхнем регистре (далее аналогично)
+            {
+                symbol = (char)(symbol + (key % 32));
+                if(symbol < 'А') {
+                    symbol = (char) (symbol + (key % 32));
+                }
+                if(symbol > 'Я') {
+                    symbol = (char) (symbol - (key % 32));
+                }
+            }
+            result.append(symbol);// Добавляем расшифрованные символы
+        }
+        return result;
     }
+
+    //Данный метод создает файл и производит запись зашифрованного текста
+
+        }
