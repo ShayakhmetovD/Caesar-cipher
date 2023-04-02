ed199be3536bb89da9645a50650d0c1ca2efa5f7
ShayakhmetovD <darkus747474@gmail.com>
2023-04-02 21:08:07 +0300

    3 этап

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,8 @@ import java.util.*;
  */
 
 public class Main {
-    char[] sym = {'.',',','"',':','-','!','?',' '};
+    static List<Character> alphabet;
+    static char[] symbols = {'.',',','"',':','-','!','?',' '};
     public static void main(String[] args) {
         try(Scanner scanner = new Scanner(System.in)){
             System.out.print("Добро пожаловать!Номера вариантов работы программы:\n" +
@@ -25,27 +26,53 @@ public class Main {
             String pathDestFile = scanner.nextLine();
             System.out.print("Введите номер варианта: ");
             int n = scanner.nextInt();
-            switch (n){
-                case 1:
+            switch (n) {
+                case 1 -> {
                     System.out.println("Вы выбрали: \"Шифрование файла с помощью ключа\"");
                     System.out.print("Введите ключ: ");
                     int encryptKey = scanner.nextInt();
-                    writingToFile(encryption(readingFromAFile(pathSourceFile),encryptKey),pathDestFile); //чтение, шифрование и запись в файл
-                    break;
-                case 2:
+                    writingToFile(encryption(readingFromAFile(pathSourceFile), encryptKey), pathDestFile); //чтение, шифрование и запись в файл
+                }
+                case 2 -> {
                     System.out.println("Вы выбрали: \"Расшифрование файла с помощью известного ключа\"");
                     System.out.print("Введите ключ: ");
                     int decryptKey = scanner.nextInt();
-                    writingToFile(decryption(readingFromAFile(pathSourceFile),decryptKey),pathDestFile); //чтение, расшифрование и запись в файл
-                    break;
-                case 3:
+                    writingToFile(decryption(readingFromAFile(pathSourceFile), decryptKey), pathDestFile); //чтение, расшифрование и запись в файл
+                }
+                case 3 -> {
                     System.out.println("Вы выбрали: \"Расшифрование файла методом brute force\"");
-                    writingToFile(bruteForce(readingFromAFile(pathSourceFile)),pathDestFile); //чтение, расшифрование и запись в файл
+                    writingToFile(bruteForce(readingFromAFile(pathSourceFile)), pathDestFile); //чтение, расшифрование и запись в файл
+                }
+                case 4 -> {
+                    System.out.println("Вы выбрали: \"Расшифрование файла на основе стастических данных\"");
+                    System.out.print("Введите путь к статистическому файлу: ");
+                    String pathUseFile = scanner.nextLine();
+                }
             }
         }catch (NullPointerException e){
             System.out.println("Пустой файл, перезапусти программу!");
         }
+    }
 
+    //Создание алфавита
+    private static List<Character> Alphabet(){
+        alphabet = new ArrayList<>();
+        for (char i = 'А'; i <= 'Я'; i++) {
+            if(i=='Е'){
+                alphabet.add(i+1,'Ё');
+            }
+            alphabet.add(i);
+        }
+        for (char i = 'а'; i <= 'я'; i++) {
+            if(i=='е'){
+                alphabet.add(i+1,'ё');
+            }
+            alphabet.add(i);
+        }
+        for (char i = 0; i < symbols.length; i++) {
+            alphabet.add(i);
+        }
+        return alphabet;
     }
 
     //Данный метод производит чтение зашифрованного/расшифрованного текста из исходного файла
@@ -64,35 +91,24 @@ public class Main {
     }
 
     //Данный метод производит шифрование исходного текста с помощью ключа
-    private static StringBuilder encryption(String sourceText, int encryptKey){
+    private static StringBuilder encryption(String sourceText, int encryptKey) {
 
         char[] array = sourceText.toCharArray();
         StringBuilder encryptedText = new StringBuilder();
+        List<Character> list = new ArrayList<>(100);
+        list = Alphabet();
         for (int i = 0; i < array.length; i++) {
             char symbol = sourceText.charAt(i);
-            if((symbol >= 'а') && (symbol <= 'я')) // если символ в нижнем регистре
-            {
-                symbol = (char)(symbol + (encryptKey % 32)); // ключ % 32 бит
-                if(symbol < 'а') {
-                    symbol = (char) (symbol + 32); // если символ стоит левее, чем начало алфавита
-                }
-                if(symbol > 'я') {
-                    symbol = (char) (symbol - (encryptKey % 32));// если символ стоит правее, чем конец алфавита
-                }
-            }else if((symbol >= 'А') && (symbol <= 'Я'))// если символ в верхнем регистре (далее аналогично)
-            {
-                symbol = (char)(symbol + (encryptKey % 32));
-                if(symbol < 'А') {
-                    symbol = (char) (symbol + (encryptKey % 32));
-                }
-                if(symbol > 'Я') {
-                    symbol = (char) (symbol - (encryptKey % 32));
-                }
+            int oldIndex = list.indexOf(symbol);
+            int newIndex = (oldIndex + encryptKey) % list.size();
+            if (newIndex < 0) {
+                newIndex = list.size() + newIndex;
             }
-            encryptedText.append(symbol);// Добавляем зашифрованные символы
+            char newSymbol = list.get(newIndex);
+            encryptedText.append(newSymbol);
         }
         return encryptedText;
-        }
+    }
 
     //Данный метод производит расшифрование зашифрованного текста с помощью известного ключа
     private static StringBuilder decryption(String sourceText, int key){
@@ -125,6 +141,29 @@ public class Main {
         return decryptedText;
     }
 
+    //Данный метод производит расшифрование текста методом brute force
+    private static StringBuilder bruteForce(String sourceText){
+        Map<Integer,Integer> map = new HashMap<>();
+        int count = 0;
+        for (int key = 0; key < Integer.MAX_VALUE; key++) {
+            String destText = decryption(sourceText,key).toString(); // создаем строку, расшифровываем текст на основе перебора ключей
+            char[] array = destText.toCharArray();
+            for (int j = 0; j < array.length; j++) {
+                char symbol = sourceText.charAt(j);
+                if(symbol == ' '){
+                    count++;
+                }
+            }
+            map.put(count,key);
+            count = 0;
+        }
+        List<Integer> list = new ArrayList<>(map.keySet());
+        Collections.sort(list);
+        Integer bruteKey = list.get(list.size() - 1);
+        System.out.println("Ваш ключ: " + bruteKey);
+        return decryption(sourceText,bruteKey);
+    }
+
     //Данный метод производит запись зашифрованного/расшифрованного текста в конечный файл
     private static void writingToFile(StringBuilder destText, String pathDestFile) {
         try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(pathDestFile))) {
@@ -133,28 +172,5 @@ public class Main {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    //Данный метод производит расшифрование текста методом brute force
-    private static StringBuilder bruteForce(String sourceText){
-        StringBuilder stringBuilder = new StringBuilder();
-        Map<Integer,Integer> map = new HashMap<>();
-        char[] array = sourceText.toCharArray();
-        int count = 0;
-        for (int key = 0; key < Integer.MAX_VALUE; key++) {
-            for (int j = 0; j < array.length; j++) {
-                char symbol = sourceText.charAt(j);
-                if(symbol == ' '){
-                    count++;
-                }
-                map.put(count,key);
-            }
-
-        }
-
-        List<Integer> list = new ArrayList<>(map.values());
-        Collections.sort(list);
-        Integer bruteKey = list.get(list.size() - 1);
-        return stringBuilder.append("Ваш ключ: ").append(bruteKey);
     }
         }
